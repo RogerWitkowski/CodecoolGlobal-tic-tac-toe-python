2d27b2897b2fc67ba20652e274f0d29c1b5737d3
RogerWitkowski <witkowski.roger@gmail.com>
2021-11-22 12:51:13 +0100

    display table

--- ver_1.py
+++ ver_1.py
@@ -1,2 +1,11 @@
-test_1 = "hello git"
-print(test_1)
+from tabulate import tabulate
+
+
+# assign data
+list = [['A', '.','.','.' ],['B','.','.','.' ],['C', '.','.','.' ] ]
+
+# create header
+head = [" ","1","2","3"]
+
+# display table
+print(tabulate(list, headers=head, tablefmt="fancy_grid"))
